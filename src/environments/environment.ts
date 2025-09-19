dcec27d955b9c13cc182d675c04a76636ea76b5b
Eduardo Uriarte <37746195+TheIonicDiamond82@users.noreply.github.com>
2025-09-18 23:12:03 -0700

    App Deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,22 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+import { initialize } from "@ionic/core";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
 
 export const environment = {
+  firebaseConfig: {
+  apiKey: "AIzaSyBhBYlWWVo6IbtpBp1P90lMnVKnlyo8wqU",
+  authDomain: "emokids-translator.firebaseapp.com",
+  projectId: "emokids-translator",
+  storageBucket: "emokids-translator.firebasestorage.app",
+  messagingSenderId: "686513720254",
+  appId: "1:686513720254:web:0c5f2385cacf17d28d87ef",
+  measurementId: "G-NEEWFT4SZY"
+  },
   production: false
 };
 
